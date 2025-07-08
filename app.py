c66803f034417e7379a685588798f9784767e57b
Narendra Manchyala <narendramanchyala@gmail.com>
2025-07-08 21:47:35 +0530

    Bind to 0.0.0.0 for Render deployment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 import random as rd
 
 app = Flask(__name__)
@@ -33,4 +34,6 @@ def index():
     return render_template("index.html", username=username)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
+
